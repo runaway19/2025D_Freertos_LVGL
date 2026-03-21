757e09ac6ea1654a6a9d6962c909c2433dcd878c
“runaway19” <2191275027@qq.com>
2026-03-21 14:30:23 +0800

    change a little bit

--- Core/Hardware/ctrl_fun.c
+++ Core/Hardware/ctrl_fun.c
@@ -1,5 +1,4 @@
 #include "ctrl_fun.h"
-#include "gpio.h"
 
 /**
  * @brief  通用 GPIO 电平切换函数
@@ -9,12 +8,30 @@
  */
 void ctrl_tx_pin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint8_t level)
 {
-	if (level) 
-	{
-		HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
-	} 
-	else 
-	{
-		HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
-	}
+		if (level) 
+		{
+				HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
+		} 
+		else 
+		{
+				HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
+		}
+}
+
+/**
+ * @brief  读取指定 GPIO 引脚的电平状态
+ * @param  GPIOx: 端口 (如 GPIOA, GPIOB)
+ * @param  GPIO_Pin: 引脚号 (如 GPIO_PIN_0)
+ * @return uint8_t: 1 为高电平, 0 为低电平
+ */
+uint8_t read_gpio_level(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    if (HAL_GPIO_ReadPin(GPIOx, GPIO_Pin) == GPIO_PIN_SET)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
--- Core/Hardware/ctrl_fun.h
+++ Core/Hardware/ctrl_fun.h
@@ -1,5 +1,11 @@
 #ifndef __CTRL_FUN_H
 #define __CTRL_FUN_H
 
+#include "gpio.h"
+
+void ctrl_tx_pin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, uint8_t level);
+uint8_t read_gpio_level(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+
+
 #endif
 
